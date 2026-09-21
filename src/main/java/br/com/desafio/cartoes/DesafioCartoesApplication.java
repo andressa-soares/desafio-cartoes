d222fc7ed3d5ab4c99b2bd8cb332a66ee40c79a0
Andressa Soares <andressasoares.dev@gmail.com>
2026-09-21 18:47:52 -0300

    docs: contrato OpenAPI do endpoint POST /cartoes

---
+++ src/main/java/br/com/desafio/cartoes/DesafioCartoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.cartoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCartoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCartoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafio/cartoes/DesafioCartoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.cartoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCartoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
